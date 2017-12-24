eea5c7c0bf83de6c76a61e324677960acc619cc6
AlexKrylov <33526162+AlexKrylov@users.noreply.github.com>
2017-12-24 22:38:52 +0300

    Хэш-функция для строки.

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,13 @@
-package PACKAGE_NAME;
-
 public class Main {
+    public static void main(String[] args) {
+        System.out.println(hash("alex"));
+    }
+    //Задание 1. Реализовать простейшую хеш-функцию.
+    public static int hash(String string) {
+        int a = 0;
+        for (int i = 0; i < string.length(); i++) {
+            a = 31 * a + string.charAt(i);
+        }
+        return a;
+    }
 }
